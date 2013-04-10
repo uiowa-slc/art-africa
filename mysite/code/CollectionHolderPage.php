a2feac0de4d808892afb4584dd022c860af91836
IMU-Drew <drewmpark@gmail.com>
2013-04-10 11:01:31 -0500

    Minor changes to results

--- mysite/code/CollectionHolderPage.php
+++ mysite/code/CollectionHolderPage.php
@@ -51,9 +51,17 @@ class CollectionHolderPage_Controller extends Page_Controller {
 		$query->search('Death');
 		//$results = singleton('MyIndex')->search($query);
 		//print_r($results);
+
 		//sleep(5);
-		print_r("HI");
-		return;
+		print_r("<br><br>LOOP<br>");
+		foreach ($results as $result){
+			print_r($result);
+			//print_r($result->ID);
+			//print_r($result->Title);
+			//print_r($result->Description);
+			print_r("HI");
+			
+		}
 	}
 
 }
