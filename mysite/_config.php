5d51b2f9b55515634ef117b5e94e243fb42bd793
Dustin Quam <quamsta@gmail.com>
2013-07-11 15:46:08 -0500

    Revert "testing SS upgrade for site tree fix"
    This reverts commit 74a521adc94a038d8e8c4936645ff20e0c038cba.

--- mysite/_config.php
+++ mysite/_config.php
@@ -30,7 +30,7 @@ Security::setDefaultAdmin('admin', 'nimlok');
 error_reporting(E_ALL);
 
 
-Director::set_environment_type("test");
+Director::set_environment_type("dev");
 //Object::add_extension('Country', "FulltextSearchable('Name,Location')");
 
 
