88af97c11daa7617c1586675d9dbc8b1b3d8a271
IMU-Drew <drewmpark@gmail.com>
2013-08-06 11:15:36 -0500

    Regex is used sloppily to transform Cory's titles into a less verbose form

--- mysite/code/FormatImageNameTask.php
+++ mysite/code/FormatImageNameTask.php
@@ -8,19 +8,56 @@ class FormatImageNameTask extends BuildTask {
     protected $enabled = true;
  
     function run($request) {
+        /*
+        
+        Here is some regex testing code
+        
+        
         $subject = "000 97 3936 CMS330 G";
+        print_r("subject = ");
+        print_r($subject);
+        print_r("<br><br>");
 		$pattern = '/[A-Z]{3}[0-9]{3}/';
-		preg_match($pattern, $subject, $matches, PREG_OFFSET_CAPTURE);
-		//$imageList = Image::get()->where('"ClassName" = "Image"');
+		preg_match($pattern, $subject, $matches, PREG_OFFSET_CAPTURE);*/
+		
 		$imageList = Image::get()->where("\"ClassName\" = 'Image'");
-		//Iterate through DataList not on template
-		/*
+		
 		$imageListIterator = $imageList->getIterator();
+		$iter = 0;
 		foreach ($imageListIterator as $image){
-			print_r($image->Title);
+			$imageTitle = $image->Title;
+			//Match IDs like CMS380, AFR101, whatever
+			$pattern = '/[A-Z]{3}[0-9]{3}/';
+			preg_match($pattern, $imageTitle, $matches, PREG_OFFSET_CAPTURE);
+			if (isset($matches[0][0])){
+				$match = $matches[0][0];
+				
+				$pattern = '/[" "][A-Z]/';
+
+				//Clearly I don't know how to do regex, because I use this to handle cases with a space and a letter after instead of combining it into the original expression
+				$imageTitleSubstr = substr($imageTitle, -2);
+				preg_match($pattern, $imageTitleSubstr, $substrMatches, PREG_OFFSET_CAPTURE);
+				if (isset($substrMatches[0][0])){
+					$match = $match . $substrMatches[0][0];
+				}
+
+				print_r($match);
+			}
+			else {
+				print_r($imageTitle);
+			}
+			
 			print_r('<br><br>');
+			$iter++;
+			/*
+			Limit iterations for testing */
+			
+			if ($iter > 500){
+				break;
+			}
+			
 		}
-		*/
+		
 		print_r($matches);
     }
 }
