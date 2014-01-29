6fb879f2e5360a5b0fbe6935ca7f572f3c7fc740
Dustin Quam <quamsta@gmail.com>
2014-01-29 08:51:20 -0600

    cleaned up config

--- mysite/_config.php
+++ mysite/_config.php
@@ -19,59 +19,44 @@ SSViewer::set_theme('simple');
 // Set the site locale
 i18n::set_locale('en_US');
 
-// Enable nested URLs for this site (e.g. page/sub-page/)
-if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
 
-error_reporting(E_ALL);
+//Shortcodes
+
+ShortcodeParser::get('default')->register(
+    'ArtPhoto', array('Page_Controller', 'artPhotoHandler')
+);
+ShortcodeParser::get('default')->register(
+    'FieldPhoto', array('Page_Controller', 'fieldPhotoHandler')
+);
+ShortcodeParser::get('default')->register(
+    'Image', array('Page_Controller', 'imageHandler')
+);
+ShortcodeParser::get('default')->register(
+    'Video', array('Page_Controller', 'videoHandler')
+);
+ShortcodeParser::get('default')->register(
+    'Audio', array('Page_Controller', 'audioHandler')
+);
+
+//Extensions
+
+Object::add_extension("DataObject","AfricaDataObjectExtension");
+Object::add_extension("Image","ImageExtension");
+Object::add_extension("AssetAdmin","AssetAdminExtension");
+
+//Enable Search
+
+FulltextSearchable::enable();
+
+//Other Settings
+
+GD::set_default_quality(90);
 
 // add a button to remove formatting
 HtmlEditorConfig::get('cms')->insertButtonsBefore(
     'styleselect',
     'removeformat'
 );
-//Object::add_extension('Country', "FulltextSearchable('Name,Location')");
-
-
-/*SearchUpdater::bind_manipulation_capture();
-Solr::configure_server(isset($solr_config) ? $solr_config : array(
-    'host' => 'localhost',
-    'indexstore' => array(
-        'mode' => 'file',
-        'path' => BASE_PATH . '/.solr'
-    )
-));
-*/
-
-ShortcodeParser::get('default')->register(
-    'ArtPhoto', array('Page_Controller', 'artPhotoHandler')
-);
-
-ShortcodeParser::get('default')->register(
-    'FieldPhoto', array('Page_Controller', 'fieldPhotoHandler')
-);
-
-ShortcodeParser::get('default')->register(
-    'Image', array('Page_Controller', 'imageHandler')
-);
-
-ShortcodeParser::get('default')->register(
-    'Video', array('Page_Controller', 'videoHandler')
-);
-
-ShortcodeParser::get('default')->register(
-    'Audio', array('Page_Controller', 'audioHandler')
-);
-
-Object::add_extension("DataObject","AfricaDataObjectExtension");
-Object::add_extension("Image","ImageExtension");
-Object::add_extension("AssetAdmin","AssetAdminExtension");
-//Object::add_extension("File","FileExtension");
-FulltextSearchable::enable();
-//Object::add_extension('CollectionPiece', "FulltextSearchable('Keywords')");
-
-Security::setDefaultAdmin("admin", "nimlok");
-
-GD::set_default_quality(90);
 
 HtmlEditorConfig::get('cms')->setOption('paste_remove_spans','true');
 HtmlEditorConfig::get('cms')->setOption('paste_auto_cleanup_on_paste','true');
