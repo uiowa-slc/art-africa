95e43160d6aa1c7967425f8ee674f156b53159e8
javeriaaali <javeriaali@dhcp80ff315a.dynamic.uiowa.edu>
2015-11-02 11:59:08 -0600

    site tree bug fix

--- mysite/code/ImageHolder.php
+++ mysite/code/ImageHolder.php
@@ -12,7 +12,7 @@ class ImageHolder extends Page {
 
   );
   
-  private static $allowed_children = array("ArtPhoto", "FieldPhoto");
+  //private static $allowed_children = array("ArtPhoto", "FieldPhoto");
   
   private static $belongs_many_many = array();
   
