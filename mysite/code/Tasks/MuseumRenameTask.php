17df774e1b95f5f078b04550d9aaa663de4d9ef4
Dustin Quam <quamsta@gmail.com>
2018-06-06 11:07:27 -0500

    added links to task

--- mysite/code/Tasks/MuseumRenameTask.php
+++ mysite/code/Tasks/MuseumRenameTask.php
@@ -13,32 +13,14 @@ class MuseumRenameTask extends BuildTask {
 		echo '<h2>Find/replacing "The University of Iowa Museum of Art" in $Caption:</h2>';
 		echo '<ul>';
 		foreach($images as $image){
-			$caption = $image->obj('Caption')->RAW();
-			//print_r($image->obj('Caption')->RAW());
-			if(strpos($caption, 'The University of Iowa Museum of Art') !== false){
-				echo '<li>Match found and replaced in... <strong>'.$image->Title.'</strong></li>';
-				$captionReplaced = str_replace('The University of Iowa Museum of Art','The University of Iowa Stanley Museum of Art', $caption);
-				
-				$image->Caption = $captionReplaced;
-				$image->write();
-			}
-
+			$this->findReplaceCaption($image, 'The University of Iowa Museum of Art', 'The University of Iowa Stanley Museum of Art');
 		}
 		echo '</ul>';
 
 		echo '<h2>Find/replacing "University of Iowa Museum of Art" in $Caption:</h2>';
 		echo '<ul>';
 		foreach($images as $image){
-			$caption = $image->obj('Caption')->RAW();
-			//print_r($image->obj('Caption')->RAW());
-			if(strpos($caption, 'University of Iowa Museum of Art') !== false){
-				echo '<li>Match found and replaced in... <strong>'.$image->Title.'</strong></li>';
-				$captionReplaced = str_replace('University of Iowa Museum of Art','The University of Iowa Stanley Museum of Art', $caption);
-				
-				$image->Caption = $captionReplaced;
-				$image->write();
-			}
-
+			$this->findReplaceCaption($image, 'University of Iowa Museum of Art', 'The University of Iowa Stanley Museum of Art');
 		}
 		echo '</ul>';
 
@@ -46,36 +28,30 @@ class MuseumRenameTask extends BuildTask {
 		echo '<ul>';
 
 		foreach($images as $image){
-			$caption = $image->obj('Caption')->RAW();
-			//print_r($image->obj('Caption')->RAW());
-			if(strpos($caption, '<p>J. Richard Simon Collection') !== false){
-				echo '<li>Match found and replaced in... <strong>'.$image->Title.'</strong></li>';
-				$captionReplaced = str_replace('<p>J. Richard Simon Collection','<p>The University of Iowa Stanley Museum of Art, J. Richard Simon Collection', $caption);
-				
-				$image->Caption = $captionReplaced;
-				$image->write();
-			}
-
+			$this->findReplaceCaption($image, '<p>J. Richard Simon Collection', '<p>The University of Iowa Stanley Museum of Art, J. Richard Simon Collection');
 		}
 		echo '</ul>';
 
 		echo '<h2>Adding UISMA to all Stanley Collection pieces:</h2>';
 		echo '<ul>';
-
 		foreach($images as $image){
+			$this->findReplaceCaption($image, '<p>The Stanley Collection', '<p>The University of Iowa Stanley Museum of Art, The Stanley Collection');
+		}
+		echo '</ul>';
+		echo '<p>Done...?</p>';
+	}
+
+
+	private function findReplaceCaption($image, $find, $replace){
 			$caption = $image->obj('Caption')->RAW();
-			//print_r($image->obj('Caption')->RAW());
-			if(strpos($caption, '<p>The Stanley Collection') !== false){
-				echo '<li>Match found and replaced in... <strong>'.$image->Title.'</strong></li>';
-				$captionReplaced = str_replace('<p>The Stanley Collection','<p>The University of Iowa Stanley Museum of Art, The Stanley Collection', $caption);
+		
+			if(strpos($caption, $find) !== false){
+				echo '<li>Match found and replaced in... <strong><a href="'.$image->ShowLink().'">'.$image->Title.'</a></strong></li>';
+				$captionReplaced = str_replace($find,$replace, $caption);
 				
 				$image->Caption = $captionReplaced;
 				$image->write();
 			}
-
-		}
-		echo '</ul>';
-		echo '<p>Done...?</p>';
 	}
  
 
